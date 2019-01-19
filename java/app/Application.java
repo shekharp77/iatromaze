90c8effc3b5cf5b41ef8fb5b5406d37c5a027305
shekhar singh <shekharp77@live.com>
2019-01-19 13:14:58 +0530

    version 0.2

--- java/app/Application.java
+++ java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ComponentScan(basePackageClasses = rest.api.Authentication.class)
 @ComponentScan(basePackageClasses = rest.api.Profile.class)
 @EnableMongoRepositories("com.repositories")
+@EnableAutoConfiguration
 public class Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
